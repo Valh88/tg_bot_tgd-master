86e87464bd85691ba60ad2c847a55a9a405d73ea
vano <simplmd@gmail.com>
2023-03-14 21:16:24 +0700

    add_pay

--- tg_bot_tgd-master/tgbot/middlewares/config_middleware.py
+++ tg_bot_tgd-master/tgbot/middlewares/config_middleware.py
@@ -1,5 +1,65 @@
-from typing import Callable, Any, Awaitable
-
+import asyncio
+from typing import Callable, Any, Awaitable, Dict
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject
-from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+from aiogram.types import TelegramObject, Message, CallbackQuery
+from aiogram.dispatcher.flags import get_flag
+from typing import Any, Awaitable, Callable, Dict
+from aiogram.dispatcher.event.handler import HandlerObject
+from cachetools import TTLCache
+
+# from bot.const import THROTTLE_TIME_SPIN, THROTTLE_TIME_OTHER
+
+THROTTLE_TIME_SPIN = 1
+THROTTLE_TIME_OTHER = 2
+
+
+class ThrottlingMiddleware(BaseMiddleware):
+    caches = {
+        "spin": TTLCache(maxsize=10_000, ttl=THROTTLE_TIME_SPIN),
+        "default": TTLCache(maxsize=10_000, ttl=THROTTLE_TIME_OTHER)
+    }
+
+    async def __call__(
+            self,
+            handler: Callable[[CallbackQuery, Dict[str, Any]], Awaitable[Any]],
+            event: CallbackQuery,
+            data: Dict[str, Any],
+    ) -> Any:
+        real_handler: HandlerObject = data.get("handler")
+        throttling_key = real_handler.flags.get("throttling_key")
+        if throttling_key is not None and throttling_key in self.caches:
+            if event.from_user.id in self.caches[throttling_key]:
+                return
+            else:
+                self.caches[throttling_key][event.from_user.id] = None
+        return await handler(event, data)
+
+
+class CallbackMiddleware(BaseMiddleware):
+
+    async def __call__(
+            self,
+            handler: Callable[[CallbackQuery, Dict[str, Any]], Awaitable[Any]],
+            event: CallbackQuery,
+            data: Dict[str, Any],
+    ) -> Any:
+        # await asyncio.sleep(2)
+        return await handler(event, data)
+
+
+class CounterMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        self.counter = 0
+
+    async def __call__(
+        self,
+        handler: Callable[[CallbackQuery, Dict[str, Any]], Awaitable[Any]],
+        event: CallbackQuery,
+        data: Dict[str, Any]
+    ) -> Any:
+        self.counter += 1
+        # print(event.from_user)
+        data['counter'] = self.counter
+        return await handler(event, data)
+
+
--- tg_bot_tgd-master/tgbot/services/rocket.py
+++ tg_bot_tgd-master/tgbot/services/rocket.py
@@ -1,4 +1,4 @@
-import json
+from typing import Optional
 
 import aiohttp
 import sys
@@ -18,17 +18,19 @@ async def get_available_currencies():
 
     url = 'https://pay.ton-rocket.com/currencies/available'
     async with aiohttp.ClientSession() as session:
-            res = await session.get(url, headers=headers)
+        res = await session.get(url, headers=headers)
     return await res.json()
 
 
 async def create_invoice_tg(
-        amount: float,
-        currency: str,
-        description: str = 'n items',
-        hidden_message: str = "thank you",
-        callback_url: str = None,
-        # expired_in: int = 86200,
+        amount: float = 0.4,
+        currency: str = 'TONCOIN',
+        # min_payment: float = 0,
+        description: str = 'описание',
+        hidden_message: str = "скрытое сообщение",
+        callback_url: str = "https://t.me/ton_rocket",
+        payload: str = "здесь то, что я увижу, когда получу чек",
+        expired_in: int = 3000,
 ):
 
     """ Одноразовая платежка """
@@ -37,14 +39,17 @@ async def create_invoice_tg(
     url = 'https://dev-pay.ton-rocket.com/tg-invoices'
     async with aiohttp.ClientSession() as session:
         data = {
-            'amount': amount,
-            'currency': currency,
-            'description': description,
-            'hiddenMessage': hidden_message,
-            'callbackUrl': callback_url,
-            # 'expiredIn': expired_in,
+            "amount": amount,
+            # "minPayment": 1.23,
+            # "numPayments": 1,
+            "currency": currency,
+            "description": description,
+            "hiddenMessage": hidden_message,
+            "callbackUrl": callback_url,
+            "payload": payload,
+            "expiredIn": expired_in
         }
-        res = await session.post(url=url, data=data, headers=headers)
+        res = await session.post(url=url, json=data, headers=headers)
         return await res.json()
 
 
@@ -53,4 +58,21 @@ async def get_all_currencies():
     async with aiohttp.ClientSession() as session:
         res = await session.get(url=url, headers=headers)
     return await res.json()
+
+
+async def delete_invoice_tg(id_invoice: int):
+    url = f'https://dev-pay.ton-rocket.com/tg-invoices/{id_invoice}'
+    async with aiohttp.ClientSession() as session:
+        res = await session.delete(url, headers=headers)
+        res = await res.json()
+        if res['success']:
+            return True
+
+
+async def get_invoice_by_id(id_invoice: int):
+    url = f'https://dev-pay.ton-rocket.com/tg-invoices/{id_invoice}'
+    async with aiohttp.ClientSession() as session:
+        res = await session.get(url, headers=headers)
+        return await res.json()
+
 
